a9497f64a095cfb2e83a1a3895073f2943770b84
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-08-21 19:48:13 -0300

    1_0_0

---
+++ CharacterRealTime/Configs_Default/Config.lua
@@ -0,0 +1,19 @@
+CharacterRealTime.Switch = true            -- true > ativa o sistema | false > desativa
+CharacterRealTime.CommandCode = 1000       -- Comando utilizado apra atualizar hardwareid
+CharacterRealTime.UpdateTick = 1           -- taxa de atualização da tabela (em segundos)
+CharacterRealTime.AttackAlertTick = 20     -- taxa de atualização da notificação de ataque (em segundos)
+CharacterRealTime.ServerName = ConfigReadString('GameServerInfo','ServerName','DATA/GameServerInfo - Common.dat') -- Não mecher ( altera nome do servidor no arquivo GameServerInfo - Common.dat )
+
+
+CharacterRealTime.Strings.hwidInvalid = {
+    "Invalid code!",
+    "Código inválido!",
+    "Codigo invalido!",
+    1
+}      
+CharacterRealTime.Strings.hwidUpdated = {
+    "Updated code!",
+    "Código atualizado!",
+    "Código actualizado!",
+    1
+}  
