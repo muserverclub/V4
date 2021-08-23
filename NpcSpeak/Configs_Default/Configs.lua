c6f2a46e69ad8a0cafd9ba2b466c9b47a4f0643e
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-08-23 17:53:29 -0300

    1_0_0

---
+++ NpcSpeak/Configs_Default/Configs.lua
@@ -0,0 +1,9 @@
+NpcSpeak.Configs = {
+    Active = true, -- 1 on, off
+    Create = {},
+    Talk = {
+        -- will affect all npc
+
+        {Npc = 230, Map = -1, MapX = -1, MapY = -1, Text = "Test2"} -- use -1 to ignore settings
+    }
+}
