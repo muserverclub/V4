a72d6d814347598cd7a4dc27beb6b85851239640
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-08-24 00:00:03 -0300

    1_0_0

---
+++ ElementalChange/Configs_Default/Configs.lua
@@ -0,0 +1,435 @@
+ElementalChange.Active = true
+
+ElementalChange.Configs = {
+    Command = {
+        Number = 500, 
+    },
+    Errtels = {6365,6375,6385,6395,6405}, -- index (section*512+ID)
+    Pentagram = {6344,6345,6346,6347,6348,6349,6350,6351,6352,6353,6354,6355,6356,6357,6358,6359,6360,6361,6362,6450,6451,6452}, -- index (section*512+ID) soon
+    Args = { -- sintax: /command 
+        [1] = "fire", -- fire
+        [2] = "water", -- water
+        [3] = "earth", -- earth
+        [4] = "wind", -- wind
+        [5] = "dark", -- wark
+    },
+    
+    PriceErrtel = { -- 
+        Rank = {
+            [1] = { -- Nível do Rank - Rank level
+
+                [1] = { -- mudar para fogo - fire
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [2] = { -- mmudar para água - water
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [3] = { -- mudar para terra - earth
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [4] = { -- mudar para vento - wind
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [5] = { -- mudar para escuridão - darkness
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+
+
+            },
+            [2] = { -- Nível do Rank - Rank level
+
+                [1] = { -- mudar para fogo
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [2] = { -- mmudar para água - water
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [3] = { -- mudar para terra - earth
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [4] = { -- mudar para vento - wind
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [5] = { -- mudar para escuridão - darkness
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                
+            },
+            [3] = { -- Nível do Rank - Rank level
+
+                [1] = { -- mudar para fogo
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [2] = { -- mmudar para água - water
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [3] = { -- mudar para terra - earth
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [4] = { -- mudar para vento - wind
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [5] = { -- mudar para escuridão - darkness
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                
+            },
+            [4] = { -- Nível do Rank - Rank level
+
+                [1] = { -- mudar para fogo
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [2] = { -- mmudar para água - water
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [3] = { -- mudar para terra - earth
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [4] = { -- mudar para vento - wind
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [5] = { -- mudar para escuridão - darkness
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                
+            },
+            [5] = {-- Nível do Rank - Rank level
+
+                [1] = { -- mudar para fogo
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [2] = { -- mmudar para água - water
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [3] = { -- mudar para terra - earth
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [4] = { -- mudar para vento - wind
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [5] = { -- mudar para escuridão - darkness
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                
+            }
+        }
+    },
+
+    PricePentagram = { -- 
+        Rank = {
+            [1] = { -- Nível do Rank - Rank level
+
+                [1] = { -- mudar para fogo - fire
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [2] = { -- mmudar para água - water
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [3] = { -- mudar para terra - earth
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [4] = { -- mudar para vento - wind
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [5] = { -- mudar para escuridão - darkness
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+
+
+            },
+            [2] = { -- Nível do Rank - Rank level
+
+                [1] = { -- mudar para fogo
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [2] = { -- mmudar para água - water
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [3] = { -- mudar para terra - earth
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [4] = { -- mudar para vento - wind
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [5] = { -- mudar para escuridão - darkness
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                
+            },
+            [3] = { -- Nível do Rank - Rank level
+
+                [1] = { -- mudar para fogo
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [2] = { -- mmudar para água - water
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [3] = { -- mudar para terra - earth
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [4] = { -- mudar para vento - wind
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [5] = { -- mudar para escuridão - darkness
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                
+            },
+            [4] = { -- Nível do Rank - Rank level
+
+                [1] = { -- mudar para fogo
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [2] = { -- mmudar para água - water
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [3] = { -- mudar para terra - earth
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [4] = { -- mudar para vento - wind
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [5] = { -- mudar para escuridão - darkness
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                
+            },
+            [5] = {-- Nível do Rank - Rank level
+
+                [1] = { -- mudar para fogo
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [2] = { -- mmudar para água - water
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [3] = { -- mudar para terra - earth
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [4] = { -- mudar para vento - wind
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                [5] = { -- mudar para escuridão - darkness
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 0,
+                    Ruud = 0,
+                    Zen = 0,
+                },
+                
+            }
+        }
+    },
+
+    Strings = {
+
+        [1] = {"[SYSTEM] Invalid item","[SYSTEM] Invalid item","[SYSTEM] Item inválido"},
+
+        [2] = {"[SYSTEM] Same element","[SYSTEM] Mesmo elemento","[SYSTEM] Mismo elemento"},
+
+        [3] = {"[SYSTEM] Invalid option! Sintax list:","[SYSTEM] Opção Inválida, lista de opções: ","[SYSTEM]Invalid option! Sintax list:"},
+
+        [4] = {"[SYSTEM] Success! New element: %s","[SYSTEM] Sucesso! Novo elemento: %s","[SYSTEM] Éxito! Nuevo elemento: %s"},
+
+        [5] =  {"[SYSTEM] Not enough cash!","[SYSTEM] Sem cash suficiente!","[SYSTEM] Sin cash!"},
+
+        [6] = {"[SYSTEM]Requirements: %d Zen, %d Ruud, %d WCoinC, %d WCoinP, %d GoblinPoints"},
+
+        [7] = {"[SYSTEM]Remove the errtels from the Pentagram!","[SYSTEM] Retire os Errtels antes de continuar","[SYSTEM]Saca los errtels antes!"},
+    }
+}
