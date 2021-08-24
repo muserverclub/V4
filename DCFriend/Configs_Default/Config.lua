609b58cb73f5774614042c9a42452bba22ef9b00
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-08-24 14:48:41 -0300

    1_0_0

---
+++ DCFriend/Configs_Default/Config.lua
@@ -0,0 +1,23 @@
+DCFriend.Status = true
+DCFriend.Command = 200
+
+DCFriend.Lang[1] = {
+    "Friend not Found/Offline",
+    "Amigo não encontrado/Offline",
+    "Amigo no existe/Offline",
+    1
+}
+
+DCFriend.Lang[2] = {
+    "Wrong Password",
+    "Senha incorreta",
+    "Clave incorrecta",
+    1
+}
+
+DCFriend.Lang[3] = {
+    "Friend has been disconnected",
+    "Amigo foi Desconectado",
+    "Amigo fue Desconectado",
+    1
+}
