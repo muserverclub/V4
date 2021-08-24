7463ce62fa30f71cff248e73f8547637a7ecd661
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-08-23 23:08:06 -0300

    1_0_0

---
+++ CustomRespawn/Configs_Default/Configs.lua
@@ -0,0 +1,27 @@
+CustomRespawn.Active = true
+
+CustomRespawn.Configs = {
+
+    { -- pode adicionar quantos quiser // can add unlimited options
+        Map         =  1    , -- mapa em que se aplica (-1 para todos os mapas) // MAP NUMBER (-1 TO IGNORE)
+        MinX        = -1    , -- Mí­nimo x do mapa (-1 para não utilizar)
+        MinY        = -1    , -- Mí­nimo y do mapa (-1 para não utilizar)
+        MaxX        = -1    , -- Máximo x do mapa (-1 para não utilizar)
+        MaxY        = -1    , -- Máximo y do mapa (-1 para não utilizar)
+        SamePlace   = 0   , -- 0 --> disable. 1 --> Respawn same place
+        Respawn     = {0,125,125} -- mapa e coordenada
+    },
+
+    {
+        Map         =  1    , -- mapa em que se aplica (-1 para todos os mapas)
+        MinX        = -1    , -- Mí­nimo x do mapa (-1 para não utilizar)
+        MinY        = -1    , -- Mí­nimo y do mapa (-1 para não utilizar)
+        MaxX        = -1    , -- Máximo x do mapa (-1 para não utilizar)
+        MaxY        = -1    , -- Máximo y do mapa (-1 para não utilizar)
+        SamePlace   = 0   , -- 0 --> disable. 1 --> Respawn same place
+        Respawn     = {0,125,125} -- mapa e coordenada
+    },
+    
+}
+
+
