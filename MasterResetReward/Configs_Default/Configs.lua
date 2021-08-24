0e7f26597094f2b27385a3124436d3a304bf10c1
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-08-23 21:50:11 -0300

    1_0_0

---
+++ MasterResetReward/Configs_Default/Configs.lua
@@ -0,0 +1,38 @@
+MasterResetReward.Switch = true
+
+MasterResetReward.Reward = {
+    
+    { -- can add infinite options
+        Min = 0, -- start range
+        Max = 999, -- end range
+        Reward = {
+            Random = 0,
+            SpecialEventBag = {},
+            RandomSpecialEventBag = {},
+            Item = {},
+            RandomItem = {},
+            ItemGive = {},
+            RandomItemGive = {},
+            Effect = {},
+            RandomEffect = {},
+            Summon = {},
+            RandomSummon = {},
+            Ruud = 0,
+            RandomRuud = {},
+            Money = 0,
+            RandomMoney = {},
+            WCoinC = 0,
+            RandomWCoinC = {},
+            WCoinP = 0,
+            RandomWCoinP = {},
+            GoblinPoint = 0,
+            RandomGoblinPoint = {},
+            QueryAccount = {},
+            QueryAccountRandom = {},
+            QueryCharacter = {},
+            QueryCharacterRandom = {}
+        }
+    },   
+
+
+}
