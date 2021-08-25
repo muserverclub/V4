b6c357620734e997fe2d17ba5f2f282f43505b45
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-08-25 11:43:57 -0300

    1_0_0

---
+++ GreedPlace/Configs_Default/Configs.lua
@@ -0,0 +1,177 @@
+GreedPlace.Switch = true -- true > enable script | false > disable
+
+GreedPlace.CommandCode = 10000 -- entrace command
+
+GreedPlace.EntraceNPC = {
+    {ID = 239, Map = 6, MapX = 139, MapY = 139, Dir = 1}
+} -- entrace npc
+
+GreedPlace.Strings.npcTitle = "..::Greed's Place::.."
+
+GreedPlace.Strings.playerLeftArea = {
+    "[Greed's Place] you left the event area",
+    "[Greed's Place] você saiu da area do evento",
+    "[Greed's Place] you left the event area",
+    33
+}
+
+GreedPlace.Strings.playerRemoved = {
+    "[Greed's Place] you were removed from the event",
+    "[Greed's Place] você foi removido do evento",
+    "[Greed's Place] you were removed from the event",
+    33
+}
+
+GreedPlace.Strings.timeAnnounce = {
+    "[Greed's Place] will start in %s minute(s) [Arena]",
+    "[Greed's Place] vai começar em %s minuto(s) [Arena]",
+    "[Greed's Place] will start in %s minute(s) [Arena]",
+    30
+}
+
+GreedPlace.Strings.timeStand = {
+    "[Greed's Place] stand time %s minute(s)",
+    "[Greed's Place] periodo de preparação %s minuto(s)",
+    "[Greed's Place] stand time %s minute(s)",
+    30
+}
+
+GreedPlace.Strings.timeRespawnMonster = {
+    "[Greed's Place] #%d wave in %d seconds",
+    "[Greed's Place] #%d wave em %d segundos",
+    "[Greed's Place] #%d wave in %d seconds",
+    30
+}
+
+GreedPlace.Strings.timeRespawnBoss = {
+    "[Greed's Place] #%d boss in %d seconds",
+    "[Greed's Place] #%d boss em %d segundos",
+    "[Greed's Place] #%d boss in %d seconds",
+    30
+}
+
+GreedPlace.Strings.timeDuration = {
+    "[Greed's Place] will end in %s minute(s)",
+    "[Greed's Place] terminará em %s minuto(s)",
+    "[Greed's Place] will end in %s minute(s)",
+    30
+}
+
+GreedPlace.Strings.timeFinish = {
+    "[Greed's Place] out in %s minute(s)",
+    "[Greed's Place] você será removido em %s minuto(s)",
+    "[Greed's Place] out in %s minute(s)",
+    30
+}
+
+GreedPlace.Strings.fewPlayers = {
+    "[Greed's Place] the minimum number of participants was not reached",
+    "[Greed's Place] o mínimo de participantes não foi atingido",
+    "[Greed's Place] the minimum number of participants was not reached",
+    30
+}
+
+GreedPlace.Strings.playerRank = {
+    "[Greed's Place] you reached the level #%d",
+    "[Greed's Place] você alcançou o nível #%d",
+    "[Greed's Place] you reached the level #%d",
+    33
+}
+
+GreedPlace.Strings.welcome = {
+    "[Greed's Place] welcome",
+    "[Greed's Place] bem vindo",
+    "[Greed's Place] welcome",
+    31
+}
+
+GreedPlace.Strings.joinAnnounce = {
+    "[Greed's Place] welcome",
+    "[Greed's Place] bem vindo",
+    "[Greed's Place] welcome",
+    31
+}
+
+GreedPlace.Strings.requirementError = {
+    "[Greed's Place] you don't meet the necessary requirements",
+    "[Greed's Place] você não possui os requerimentos necessarios",
+    "[Greed's Place] you don't meet the necessary requirements",
+    33
+}
+
+GreedPlace.Strings.participantsLimit = {
+    "[Greed's Place] %d participants limit exceded",
+    "[Greed's Place] limite de %d participantes atingido",
+    "[Greed's Place] %d participants limit exceded",
+    33
+}
+
+GreedPlace.Strings.hardwareLimitGlobal = {
+    "[Greed's Place] limit of %d simultaneous events exceeded",
+    "[Greed's Place] limite de %d eventos simultâneos excedido",
+    "[Greed's Place] limit of %d simultaneous events exceeded",
+    33
+}
+
+GreedPlace.Strings.hardwareLimitPerEvent = {
+    "[Greed's Place] limit of %d simultaneous entraces exceeded",
+    "[Greed's Place] limite de %d entradas simultâneos excedido",
+    "[Greed's Place] limit of %d simultaneous entraces exceeded",
+    33
+}
+
+GreedPlace.Strings.ipLimitGlobal = {
+    "[Greed's Place] limit of %d simultaneous events exceeded",
+    "[Greed's Place] limite de %d eventos simultâneos excedido",
+    "[Greed's Place] limit of %d simultaneous events exceeded",
+    33
+}
+
+GreedPlace.Strings.ipLimitPerEvent = {
+    "[Greed's Place] limit of %d simultaneous entraces exceeded",
+    "[Greed's Place] limite de %d entradas simultâneos excedido",
+    "[Greed's Place] limit of %d simultaneous entraces exceeded",
+    33
+}
+
+GreedPlace.Strings.entraceMax = {
+    "[Greed's Place] limit of %d entraces exceeded",
+    "[Greed's Place] limite de %d entradas excedido",
+    "[Greed's Place] limit of %d entraces exceeded",
+    33
+}
+
+GreedPlace.Strings.entraceClosed = {
+    "[Greed's Place] closed entrance",
+    "[Greed's Place] entrada fechada",
+    "[Greed's Place] closed entrance",
+    33
+}
+
+GreedPlace.Strings.userdieAnnounce = {
+    "[Greed's Place] %s killed by %s",
+    "[Greed's Place] %s morto por %s",
+    "[Greed's Place] %s killed by %s",
+    33
+}
+
+GreedPlace.Strings.killInfoCount = {
+    "[Greed's Place] kills %d",
+    "[Greed's Place] abates %d",
+    "[Greed's Place] kills %d",
+    31
+}
+
+GreedPlace.Strings.deathInfoCount = {
+    "[Greed's Place] deaths [ %d | %d ]",
+    "[Greed's Place] mortes [ %d | %d ]",
+    "[Greed's Place] deaths [ %d | %d ]",
+    31
+}
+
+GreedPlace.Strings.eventCancel = {
+    "[Greed's Place] event canceled",
+    "[Greed's Place] evento cancelado",
+    "[Greed's Place] event canceled",
+    30
+}
