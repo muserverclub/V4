849869bf8b7a70280a98afa92409bacf2a8496e1
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-08-25 11:36:09 -0300

    1_0_0

---
+++ CheckOnMoveUser/Configs_Default/Config.lua
@@ -0,0 +1,11 @@
+CheckOnMoveUser.Switch = true -- true > enable | false > disable
+
+-- Add a verification
+CheckOnMoveUser.List.NameOfThisVerification = {
+  Requeriments = {
+    displayErrors = false,
+    AllowMaps = {3}
+  }, -- Requeriment list | https://github.com/muserverclub/ServerClub-Resources/tree/main/Guides%20(Guias)/Npc-Command-Requirement-Guide
+  Reward = {}
+  -- Reward actions | https://github.com/muserverclub/ServerClub-Resources/tree/main/Guides%20(Guias)/Reward-Guide
+}
