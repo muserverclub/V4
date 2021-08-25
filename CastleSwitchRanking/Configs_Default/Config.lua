92ebbd4364604b1b69d3ca69e51b3f1db9cccc02
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-08-25 11:32:59 -0300

    1_0_0

---
+++ CastleSwitchRanking/Configs_Default/Config.lua
@@ -0,0 +1,5 @@
+CastleSwitchRanking.Switch = true -- true > enable | false > disable
+CastleSwitchRanking.GameServerCode = 19 -- Gameserver number the script will work on
+CastleSwitchRanking.Schedule = {
+  {hour = 16, min = 55, sec = 0}
+}
