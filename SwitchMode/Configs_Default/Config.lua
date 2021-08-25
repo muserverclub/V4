770d700bee9a6857dd2925b04051d334d0df894c
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-08-25 12:26:11 -0300

    1_0_0

---
+++ SwitchMode/Configs_Default/Config.lua
@@ -0,0 +1,3 @@
+SwitchMode.Switch = true -- true > enable | false > disable
+SwitchMode.displayErrors = false  -- Not display errors
+SwitchMode.AllowAccountLevel = {1, 2, 3} -- Enabled Account Level
