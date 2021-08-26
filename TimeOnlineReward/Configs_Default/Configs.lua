d06645fb943af6fb2285c0f9f2e5a5837cfca944
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-08-26 11:03:06 -0300

    1_0_0

---
+++ TimeOnlineReward/Configs_Default/Configs.lua
@@ -0,0 +1,71 @@
+TimeOnlineReward.Configs = { 
+    Active = true,   
+    OnCommand = {
+        [1] = {
+            CommandNumber = 200,
+            GmLevel = 1, -- GMLevel on Util/GameMaster
+            Reward = {
+                Random = 0,
+                SpecialEventBag = {},
+                RandomSpecialEventBag = {},
+                Item = {},
+                RandomItem = {},
+                ItemGive = {},
+                RandomItemGive = {},
+                Effect = {},
+                RandomEffect = {},
+                Summon = {},
+                RandomSummon = {},
+                Ruud = 0,
+                RandomRuud = {},
+                Money = 0,
+                RandomMoney = {},
+                WCoinC = 0,
+                RandomWCoinC = {},
+                WCoinP = 0,
+                RandomWCoinP = {},
+                GoblinPoint = 0,
+                RandomGoblinPoint = {},
+                QueryAccount = {},
+                QueryAccountRandom = {},
+                QueryCharacter = {},
+                QueryCharacterRandom = {}
+            }
+        }
+    },
+    Automatic = {         
+
+        [1] = {
+            Time = 60, -- Minutes required to receive the reward
+            Reward = {
+                Random = 0,
+                SpecialEventBag = {},
+                RandomSpecialEventBag = {},
+                Item = {},
+                RandomItem = {},
+                ItemGive = {},
+                RandomItemGive = {},
+                Effect = {},
+                RandomEffect = {},
+                Summon = {},
+                RandomSummon = {},
+                Ruud = 0,
+                RandomRuud = {},
+                Money = 0,
+                RandomMoney = {},
+                WCoinC = 0,
+                RandomWCoinC = {},
+                WCoinP = 0,
+                RandomWCoinP = {},
+                GoblinPoint = 0,
+                RandomGoblinPoint = {},
+                QueryAccount = {},
+                QueryAccountRandom = {},
+                QueryCharacter = {},
+                QueryCharacterRandom = {}
+            }
+        },
+    }
+
+    
+}
---
+++ TimeOnlineReward/Query.sql
@@ -0,0 +1,4 @@
+alter table memb_info add Minutes int not null default (0)
+alter table Character add Minutes int not null default (0)
+alter table Character add MinutesWek int not null default (0)
+alter table Character add MinutesMon int not null default (0)
