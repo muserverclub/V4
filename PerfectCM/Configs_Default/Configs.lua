adf884a22ded21c8b3810510598002dfff689803
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-08-26 11:08:41 -0300

    1_0_0

---
+++ PerfectCM/Configs_Default/Configs.lua
@@ -0,0 +1,20 @@
+PerfectCM.Active = true
+
+PerfectCM.BlockItems = {7122,7123} -- itens que não serão restaurados (pena, partes...)
+PerfectCM.JewelIndex = {(512*12+15),(512*14+13),(512*14+14),(512*14+16),(512*14+22),(512*14+31),(512*14+41),(512*14+42),(512*14+43),(512*14+44)} -- joias e itens de combinação 
+PerfectCM.String = {
+
+    {
+        "[SYSTEM]Your item has been restored, but it lost One Level",
+        "[SYSTEM]Sem item foi restaurado, mas ele perdeu Um Level",
+        "[SYSTEM]Su item fue restaurado, pero perdió Un Nivel",
+    },
+
+    {
+        "[SYSTEM]Your combination will be protected for losing items",
+        "[SYSTEM]Sua combinação será protegida para não quebrar itens",
+        "[SYSTEM]Su combinación serpa protegida para romper items",
+    },
+
+    
+}
