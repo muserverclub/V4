c47beb01b9515877ff14b18590d682e79c4a9687
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-08-26 00:54:18 -0300

    1_0_0

---
+++ ErrtelRankUpgrade/Configs_Default/Configs.lua
@@ -0,0 +1,488 @@
+ErrtelRankUpgrade.Active = true
+
+ErrtelRankUpgrade.Index = {6365,6375,6385,6395,6405} -- all errtel index
+ErrtelRankUpgrade.NpcList = {
+    --{NPC-NUMBER,MAP-NUMBER,MAPX,MAPY,POSITION,TEXT}
+    {239,0,139,127,1,"UPGRADE ERRTEL RANK"}
+}
+
+ErrtelRankUpgrade.MustHave2 = 1 -- 0 disable, 1 enable. To upgrade errtel must have 2 off same type (1st and seccond inventory slot)
+
+ErrtelRankUpgrade.Errtel = {
+
+    ["Fire"] = {
+
+        [1] = { -- Rank 1 to Rank 2
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+        [2] = { -- Rank 2 to Rank 3
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+        [3] = { -- Rank 3 to Rank 4
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+        [4] = { -- Rank 4 to Rank 5
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+    },
+
+    ["Water"] = {
+
+        [1] = { -- Rank 1 to Rank 2
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+        [2] = { -- Rank 2 to Rank 3
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+        [3] = { -- Rank 3 to Rank 4
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+        [4] = { -- Rank 4 to Rank 5
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+    },
+
+    ["Earth"] = {
+
+        [1] = { -- Rank 1 to Rank 2
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+        [2] = { -- Rank 2 to Rank 3
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+        [3] = { -- Rank 3 to Rank 4
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+        [4] = { -- Rank 4 to Rank 5
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+    },
+
+    ["Wind"] = {
+
+        [1] = { -- Rank 1 to Rank 2
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+        [2] = { -- Rank 2 to Rank 3
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+        [3] = { -- Rank 3 to Rank 4
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+        [4] = { -- Rank 4 to Rank 5
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+    },
+
+    ["Darkness"] = {
+
+        [1] = { -- Rank 1 to Rank 2
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+        [2] = { -- Rank 2 to Rank 3
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+        [3] = { -- Rank 3 to Rank 4
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+        [4] = { -- Rank 4 to Rank 5
+            Price = { -- Payment Option
+
+                { -- First payment option
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 100,
+                    WCoinP = 0,
+                    GoblinPoint = 0,                        
+                },
+
+                { -- Second payment option (if player cant pay the first option)
+                    Zen = 0,
+                    Ruud = 0,
+                    WCoinC = 0,
+                    WCoinP = 0,
+                    GoblinPoint = 100,                        
+                },
+
+            }
+        },
+
+    },
+    
+}
+
+ErrtelRankUpgrade.Strings = {
+    [1] = {"Item must be on fisrt slot","Item precisa estarno primeiro slot","El item necesita estar en el 1 slot"},
+    [2] = {"Ivalid item","Item inválido","Item invalido"},
+    [3] = {"Not enough CASH! Payment options:","Sem cash suficiente! Opções de pagamento","Le falta plata. Opciones de pago:"},
+    [4] = {"%d- %d WCoinC, %d WCoinP, %d GoblinPoint, %d Ruud, %d Zen"},
+    [5] = {"Click again to continue","Clique para continuar","Click para continuar"},
+    [6] = {"Errtel Upgrade: Element %s - Rank %d to Rank %d"},
+    [7] = {"[SYSTEM]Errtel Max Rank 5!"},
+    [8] = {"Other Errtel required on seccond slot","Precisa de outro Errtel no slot 2","Necesita otro errtel en el segundo slot"},
+    [9] = {"Seccond errtel must be the same of the first","Segundo Errtel precisa ser igual ao primeiro","El segundo Errtel necesita ser igual al primer"},
+    [10] = {"Errtel must be at least +7 to Rank UP","Errtel precisa ser pelo menos +7","Errtel necesita ser por lo menos +7"},
+    [11] = {"[SYSTEM] Errtel was upgraded to Rank %d", "[SYSTEM] Errtel foi atualizado para Rank %d","[SYSTEM]Errtel fue evoluído para Rank %d"},
+    [12] = {"Seccond Errtel must be at least +7 to Rank UP","Seccond Errtel precisa ser pelo menos +7","Seccond Errtel necesita ser por lo menos +7"},
+}
