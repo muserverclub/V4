a337b56ce6500af5e710b84e69531d73bc526aad
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-08-31 15:57:19 -0300

    1_1_0

--- PKFree/Configs_Default/Config.lua
+++ PKFree/Configs_Default/Config.lua
@@ -1,62 +1,64 @@
 PKFree.Switch = true -- true > enable | false > disable
-PKFree.AutoSetPK = true -- true > Automatically add PK so you don't have to click ctrl | false > not applie
-PKFree.displayErrors = false
 
-PKFree.AllowMaps = {
-	24,
-	25,
-	26,
-	27,
-	28,
-	29,
-	36
-}
--- List of maps where it will take effect
-PKFree.AllowCoord = {
+table.insert(
+	PKFree.List,
 	{
-		Map = 58,
-		MapXMin = 1,
-		MapXMax = 255,
-		MapYMin = 1,
-		MapYMax = 255
-	},
-	{
-		Map = 100,
-		MapXMin = 0,
-		MapXMax = 255,
-		MapYMin = 0,
-		MapYMax = 255
-	},
-	{
-		Map = 110,
-		MapXMin = 105,
-		MapXMax = 185,
-		MapYMin = 105,
-		MapYMax = 185
-	},
-	{
-		Map = 112,
-		MapXMin = 4,
-		MapXMax = 44,
-		MapYMin = 103,
-		MapYMax = 143
-	},
-	{
-		Map = 113,
-		MapXMin = 10,
-		MapXMax = 50,
-		MapYMin = 198,
-		MapYMax = 238
-	},
-	{
-		Map = 122,
-		MapXMin = 164,
-		MapXMax = 204,
-		MapYMin = 1,
-		MapYMax = 38
+		Schedule = {{}}, -- time when pk free mode is activated
+		Duration = 86400, -- pkfree duration
+		AutoSetPK = true, -- true > enable pk on all players in the area | false > not active
+		HitSameGuild = true, -- true > allows players of the same guild to attack each other | false > does not allow
+		HitSameAlliance = true, -- true > allows players of the same alliance to attack each other | false > does not allow
+		HitSameGens = true, -- true > allows players of the same gen to attack each other | false > does not allow
+		HitSameParty = true, -- true > allows players in the same party to attack each other | false > does not allow
+		StayRequeriments = {},
+		-- who can stay
+		RemoveTo = false, -- false > removes players who do not meet StayRequests | destination {Map = 0,MapX = 125,MapY = 125} | destination {Map = 0,MapXMin = 125,MapYMin = 125,,MapXMax = 125,MapYMax = 125}
+		AreaRequirements = {
+			displayErrors = false,
+			AllowMaps = {
+				24,
+				25,
+				26,
+				27,
+				28,
+				29,
+				36,
+				58,
+				100
+			},
+			AllowCoord = {
+				{
+					Map = 110,
+					MapXMin = 105,
+					MapXMax = 185,
+					MapYMin = 105,
+					MapYMax = 185
+				},
+				{
+					Map = 112,
+					MapXMin = 4,
+					MapXMax = 44,
+					MapYMin = 103,
+					MapYMax = 143
+				},
+				{
+					Map = 113,
+					MapXMin = 10,
+					MapXMax = 50,
+					MapYMin = 198,
+					MapYMax = 238
+				},
+				{
+					Map = 122,
+					MapXMin = 164,
+					MapXMax = 204,
+					MapYMin = 1,
+					MapYMax = 38
+				}
+			}
+		} -- Pkfree area activation requirements
 	}
-}
---List of places where the script takes effect
+)
 
 PKFree.Strings.EnterPVPZone = {
 	"You have entered a PvP zone, your PK level has changed.",
