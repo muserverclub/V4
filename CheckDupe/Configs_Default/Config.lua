c2f2491bfde58236cc498022df2f524984397462
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-09-03 16:47:34 -0300

    1_1_0

--- CheckDupe/Configs_Default/Config.lua
+++ CheckDupe/Configs_Default/Config.lua
@@ -1,16 +1,15 @@
 CheckDupe.Switch = true -- true > enable system | false > disabled
+CheckDupe.SendMessage = true -- Send message to player
+CheckDupe.BlockItems = false -- Block user inventory
+CheckDupe.SaveLog = true -- Save Logs of dupped items
+CheckDupe.DeleteItems = true -- Delete dupped items
+
 CheckDupe.Action = function(aIndex, list)
-  if #list > 0 then
-    NoticeSend(
-      aIndex,
-      {
-        "You have a dupped items, contact an administrator",
-        "Você possui items dupado, contate o administrador",
-        "Usted tiene dupped items, contacte el administrador",
-        3
-      }
-    )
-    PermissionRemove(aIndex, 1)
-    PermissionRemove(aIndex, 2)
-  end
-end
+end -- Custom action
+
+CheckDupe.Strings.dupeFound = {
+  "You have a dupped items, contact an administrator",
+  "Você possui items dupado, contate o administrador",
+  "Usted tiene dupped items, contacte el administrador",
+  3
+}
