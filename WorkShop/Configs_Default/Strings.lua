243a14d8cfa237d2ed87700d7e4c9bdf64a6f098
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-09-05 00:48:56 -0300

    1_0_0

---
+++ WorkShop/Configs_Default/Strings.lua
@@ -0,0 +1,148 @@
+
+WorkShop.String[4] = {"You cannot update the level of this item","Você não pode atualizar o level desse item","No puedes actualizar el nivel de este artículo.",3}
+WorkShop.String[5] = {"Maximum level already reached, use the chaos machine","Level máximo já atingido, use a chaos machine","Nivel máximo ya alcanzado, usa la chaos machine",3}
+WorkShop.String[6] = {"(Level Upgrade):","(Level Upgrade):","(Mejora de nivel):",3}
+WorkShop.String[7] = {" > %d ".._coin.name.WCoinC.." (Rate: %d%%): insert %d to +%d"," > %d ".._coin.name.WCoinC.." (Rate: %d%%): insira %d para +%d"," >% d (Tasa: %d%%): ingrese %d para +%d:",1}
+WorkShop.String[8] = {"This item can only be updated when permanent","Esse item só pode ser atualizado quando permanente","Este artículo solo se puede actualizar cuando es permanente",3}
+WorkShop.String[9] = {"You don't have %d ".._coin.name.WCoinC,"Você não tem %d ".._coin.name.WCoinC,"No tienes %d ".._coin.name.WCoinC,3}
+WorkShop.String[10] = {"Item update failed","Falha na atualização do item","Error al actualizar el artículo",3}
+WorkShop.String[11] = {"Level updated to %d","Level atualizado para +%d","Nivel actualizado a %d",1}
+
+WorkShop.String[12] = {"You cannot add skill on that item","Você não pode adicionar skill nesse item","No puedes agregar habilidad en ese elemento",3}
+WorkShop.String[13] = {"The item already has skill","O item já possui skill","El objeto ya tiene habilidad",3}
+WorkShop.String[14] = {"(Skill Upgrade):","(Skill Upgrade):","(Mejora de habilidad):",3}
+WorkShop.String[15] = {" > %d ".._coin.name.WCoinC.." (Rate: %s%%): enter 1 to confirm"," > %d ".._coin.name.WCoinC.." (Rate: %s%%): insira 1 para confirmar"," > %d ".._coin.name.WCoinC.." (Tasa: %s%%): ingrese 1 para confirmar",1}
+WorkShop.String[16] = {"This item can only be updated when permanent","Esse item só pode ser atualizado quando permanente","Este artículo solo se puede actualizar cuando es permanente",3}
+WorkShop.String[17] = {"You don't have %d ".._coin.name.WCoinC,"Você não tem %d ".._coin.name.WCoinC,"No tienes %d ".._coin.name.WCoinC,3}
+WorkShop.String[18] = {"Item update failed","Falha na atualização do item","Error al actualizar el artículo",3}
+WorkShop.String[19] = {"Skill added","Skill adicionado","Habilidad agregada",1}
+
+WorkShop.String[20] = {"You cannot add luck to that item","Você não pode adicionar luck neste item","No puedes agregar suerte a ese artículo",3}
+WorkShop.String[21] = {"The item already has luck","O item já possui luck","El artículo ya tiene suerte",3}
+WorkShop.String[22] = {"(Luck Upgrade):","(Luck Upgrade):","(Luck Upgrade):",3}
+WorkShop.String[23] = {" > %d ".._coin.name.WCoinC.." (Rate: %s%%): enter 1 to confirm"," > %d ".._coin.name.WCoinC.." (Rate: %s%%): insira 1 para confirmar","> %d (Tasa: %s%%): ingrese 1 para confirmar",1}
+WorkShop.String[24] = {"This item can only be updated when permanent","Esse item só pode ser atualizado quando permanente","Este artículo solo se puede actualizar cuando es permanente",3}
+WorkShop.String[25] = {"You don't have %d ".._coin.name.WCoinC,"Você não tem %d ".._coin.name.WCoinC,"No tienes %d ".._coin.name.WCoinC,3}
+WorkShop.String[26] = {"Item update failed","Falha na atualização do item","Error al actualizar el artículo",3}
+WorkShop.String[27] = {"Luck adicionado","Luck adicionado","Luck añadido",1}
+
+WorkShop.String[28] = {"You cannot update the health of that item","Você não pode atualizar o life deste item","No puedes actualizar la salud de ese artículo.",3}
+WorkShop.String[29] = {"Maximum life already reached","Life máximo já atingido","Vida máxima ya alcanzada",3}
+WorkShop.String[30] = {"(Life Upgrade):","(Life Upgrade):","(Mejora de vida):",3}
+WorkShop.String[31] = {" > %d ".._coin.name.WCoinC.." (Rate: %d%%): enter %d for +%d"," > %d ".._coin.name.WCoinC.." (Rate: %d%%): insira %d para +%d"," > %d ".._coin.name.WCoinC.." (Tasa: %d%%): ingrese %d para +%d",1}
+WorkShop.String[32] = {"This item can only be updated when permanent","Esse item só pode ser atualizado quando permanente","Este artículo solo se puede actualizar cuando es permanente",3}
+WorkShop.String[33] = {"You don't have %d ".._coin.name.WCoinC,"Você não tem %d ".._coin.name.WCoinC,"No tienes %d ".._coin.name.WCoinC,3}
+WorkShop.String[34] = {"Item update failed","Falha na atualização do item","Error al actualizar el artículo",3}
+WorkShop.String[35] = {"Life updated to +%d","Life atualizado para +%d","Vida actualizada a +%d",1}
+
+WorkShop.String[36] = {"You cannot update the excellent of that item","Você não pode atualizar o excellent deste item","No se puede actualizar el excelente de ese artículo.",3}
+WorkShop.String[37] = {"Excellent maximum already reached","Excellent máximo já atingido","Excelente máximo ya alcanzado",3}
+WorkShop.String[38] = {"(Excellent Upgrade):","(Excellent Upgrade):","(Excelente actualización):",3}
+WorkShop.String[39] = {" > %d ".._coin.name.WCoinC.." (Rate: %d%%): enter %d for %s"," > %d ".._coin.name.WCoinC.." (Rate: %d%%): insira %d para %s"," > %d ".._coin.name.WCoinC.." (Tasa: %d%%): ingrese %d para %s",1}
+WorkShop.String[40] = {"This item can only be updated when permanent","Esse item só pode ser atualizado quando permanente","Este artículo solo se puede actualizar cuando es permanente",3}
+WorkShop.String[41] = {"You don't have %d ".._coin.name.WCoinC,"Você não tem %d ".._coin.name.WCoinC,"No tienes %d ".._coin.name.WCoinC,3}
+WorkShop.String[42] = {"Item update failed","Falha na atualização do item","Error al actualizar el artículo",3}
+WorkShop.String[43] = {"Excellente added: %s","Excellente adicionado: %s","Excellente agregado: %s",1}
+
+
+WorkShop.String[44] = {"You cannot update the ancient of that item","Você não pode atualizar o ancient desse item","No puedes actualizar la antigüedad de ese artículo.",3}
+WorkShop.String[45] = {"The item is already ancient","O item já é ancient","El artículo ya es antiguo",3}
+WorkShop.String[46] = {"(Ancient Upgrade):","(Ancient Upgrade):","(Actualización antigua):",3}
+WorkShop.String[47] = {" > %d ".._coin.name.WCoinC.." (Rate: %d%%): enter %d for %s"," > %d ".._coin.name.WCoinC.." (Rate: %d%%): insira %d para %s"," > %d ".._coin.name.WCoinC.." (Tasa: %d%%): ingrese %d para %s",1}
+WorkShop.String[48] = {"This item can only be updated when permanent","Esse item só pode ser atualizado quando permanente","Este artículo solo se puede actualizar cuando es permanente",3}
+WorkShop.String[49] = {"You don't have %d ".._coin.name.WCoinC,"Você não tem %d ".._coin.name.WCoinC,"No tienes %d ".._coin.name.WCoinC,3}
+WorkShop.String[50] = {"Item update failed","Falha na atualização do item","Error al actualizar el artículo",3}
+WorkShop.String[51] = {"Ancient added: %s","Ancient adicionado: %s","Antiguo agregado: %s",1}
+
+WorkShop.String[52] = {"You cannot update the harmony of this item","Você não pode atualizar o harmony deste item","No puedes actualizar la armonía de este artículo.",3}
+WorkShop.String[53] = {"(Harmony Upgrade) Options:","(Harmony Upgrade) Opções:","(Actualización de Harmony) Opciones:",3}
+WorkShop.String[54] = {" > enter %d for %s"," > insira %d para %s"," > ingrese %d para %s",1}
+WorkShop.String[55] = {"(Harmony Upgrade):","(Harmony Upgrade):","(Actualización de armonía):",3}
+WorkShop.String[56] = {" >> Options for: %s"," >> Opções para: %s"," >> Opciones para: %s",3}
+WorkShop.String[57] = {" > %d ".._coin.name.WCoinC.." (Rate: %d%%): enter %d for +%d"," > %d ".._coin.name.WCoinC.." (Rate: %d%%): insira %d para +%d"," >% d (Tasa: %d%%): ingrese %d para +%d",1}
+WorkShop.String[58] = {"You have already reached the maximum level in this option","Você já alcançou o nível máximo nessa opção","Ya has alcanzado el nivel máximo en esta opción.",1}
+WorkShop.String[59] = {"This item can only be updated when permanent","Esse item só pode ser atualizado quando permanente","Este artículo solo se puede actualizar cuando es permanente",3}
+WorkShop.String[60] = {"You don't have %d ".._coin.name.WCoinC,"Você não tem %d ".._coin.name.WCoinC,"No tienes %d ".._coin.name.WCoinC,3}
+WorkShop.String[61] = {"Item update failed","Falha na atualização do item","Error al actualizar el artículo",3}
+WorkShop.String[62] = {"Harmony added: %s +%d","Harmony adicionado: %s +%d","Armonía añadida: %s +%d",1}
+
+WorkShop.String[63] = {"You cannot add 380 refine to that item","Você não pode adicionar refine 380 nesse item","No puede agregar 380 refinar a ese elemento",3}
+WorkShop.String[64] = {"The item already has refine 380","O item já possui refine 380","El artículo ya tiene refine 380",3}
+WorkShop.String[65] = {"(Refine 380 Upgrade) (Rate: %s%%):","(Refine 380 Upgrade) (Rate: %s%%):","(Refine 380 Upgrade) (Tasa: %s%%):",3}
+WorkShop.String[66] = {" >> %s | %s"," >> %s | %s"," >> %s | %s",3}
+WorkShop.String[67] = {" > %d ".._coin.name.WCoinC..": enter 1 to confirm"," > %d ".._coin.name.WCoinC..": insira 1 para confirmar"," > %d ".._coin.name.WCoinC..": ingrese 1 para confirmar",1}
+WorkShop.String[68] = {"This item can only be updated when permanent","Esse item só pode ser atualizado quando permanente","Este artículo solo se puede actualizar cuando es permanente",3}
+WorkShop.String[69] = {"You don't have %d ".._coin.name.WCoinC,"Você não tem %d ".._coin.name.WCoinC,"No tienes %d ".._coin.name.WCoinC,3}
+WorkShop.String[70] = {"Item update failed","Falha na atualização do item","Error al actualizar el artículo",3}
+WorkShop.String[71] = {"Refine added","Refine adicionado","Refinar agregado",1}
+
+
+WorkShop.String[72] = {"You cannot update the Socket for this item","Você não pode atualizar o Socket desse item","No puede actualizar el enchufe para este artículo",3}
+WorkShop.String[73] = {"(Socket Upgrade) Slots:","(Socket Upgrade) Slots:","(Actualización de socket) Ranuras:",3}
+WorkShop.String[74] = {" > enter %d to manage slot %d"," > insira %d para gerenciar o slot %d"," > ingrese %d para administrar el espacio %d",1}
+WorkShop.String[75] = {"You need to purchase the %d slot before","Você precisa adquirir o slot %d antes","Debes comprar la ranura %d antes",3}
+WorkShop.String[76] = {"(Socket Upgrade) Slot options %d:","(Socket Upgrade) Opções Slot %d:","(Actualización de socket) Opciones de ranura %d:",3}
+WorkShop.String[77] = {" > enter %d for %s"," > insira %d para %s"," > ingrese %d para %s",1}
+WorkShop.String[78] = {"(Socket Upgrade) Slot Options %d:","(Socket Upgrade) Opções Slot %d:","(Actualización de socket) Opciones de ranura %d:",3}
+WorkShop.String[79] = {" ~ %s:"," ~ %s:"," ~ %s:",3}
+WorkShop.String[80] = {" > %d ".._coin.name.WCoinC.." (Rate: %d%%): insert %d for %s +%d"," > %d ".._coin.name.WCoinC.." (Rate: %d%%): insira %d para %s +%d"," > %d ".._coin.name.WCoinC.." (Tasa: %d%%): inserte %d para %s +%d",1}
+WorkShop.String[81] = {"Maximum level reached","Level máximo alcançado","Level máximo alcançado",1}
+WorkShop.String[82] = {"This item can only be updated when permanent","Esse item só pode ser atualizado quando permanente","Este artículo solo se puede actualizar cuando es permanente",3}
+WorkShop.String[83] = {"You don't have %d ".._coin.name.WCoinC,"Você não tem %d ".._coin.name.WCoinC,"No tienes %d ".._coin.name.WCoinC,3}
+WorkShop.String[84] = {"Updated %d socket: %s +%d","Socket %d atualizado: %s +%d","Socket %d actualizado: %s +%d",1}
+
+WorkShop.String[85] = {"The item does not have an excellent option","O item não possui opção excellent","El artículo no tiene una excelente opción.",3}
+WorkShop.String[86] = {"You cannot remove the excellent from that item","Você não pode remover o excellent desse item","No puedes quitar lo excelente de ese artículo.",3}
+WorkShop.String[87] = {"You don't have %d ".._coin.name.WCoinC,"Você não tem %d ".._coin.name.WCoinC,"No tienes %d ".._coin.name.WCoinC,3}
+WorkShop.String[88] = {"Excellent %s removed","Excllente %s removido","Excelente %s eliminado",1}
+
+
+WorkShop.String[89] = {"The item must be in slot 1","O item precisa estar no slot 1","El artículo debe estar en la ranura 1",3}
+WorkShop.String[90] = {"The inserted item cannot be updated","O item inserido não pode ser atualizado","El elemento insertado no se puede actualizar",3}
+
+WorkShop.String[91] = {"Close all windows to use this command.","Feche todas as janelas para usar esse comando.","Cierre todas las ventanas para usar este comando.",3}
+
+
+WorkShop.String[92] = {"(Excellent Remove):","(Excellent Remove):","(Eliminar Excellent):",3}
+WorkShop.String[93] = {" > %d ".._coin.name.WCoinC.." - enter %d for %s"," > %d ".._coin.name.WCoinC.." - insira %d para %s"," > %d ".._coin.name.WCoinC.." - ingrese %d para %s",1}
+WorkShop.String[94] = {"Invalid option","Opção inválida","Opción inválida",3}
+WorkShop.String[95] = {"This item can only be updated when permanent","Esse item só pode ser atualizado quando permanente","Este artículo solo se puede actualizar cuando es permanente",3}
+
+WorkShop.String[96] = {"Combination %s + skill not allowed","Combinação %s + skill não permitida","Combinación %s + skill no permitida",3}
+WorkShop.String[97] = {"Combination %s + luck not allowed","Combinação %s + luck não permitida","Combinación %s + luck no permitida",3}
+WorkShop.String[98] = {"Combination %s + excellent not allowed","Combinação %s + excellent não permitida","Combinación %s + excellent no permitida",3}
+WorkShop.String[99] = {"Combination %s + harmony not allowed","Combinação %s + harmony não permitida","Combinación %s + harmony no permitida",3}
+WorkShop.String[100] = {"Combination %s + ancient not allowed","Combinação %s + ancient não permitida","Combinación %s + ancient no permitida",3}
+WorkShop.String[101] = {"Combination %s + refine not allowed","Combinação %s + refine não permitida","Combinación %s + refine no permitida",3}
+WorkShop.String[102] = {"Combination %s + socket not allowed","Combinação %s + socket não permitida","Combinación %s + socket no permitida",3}
+
+
+WorkShop.String[103] = {"You cannot update the ruud option of that item","Você não pode atualizar o ruud option deste item","No se puede actualizar el ruud option de ese artículo.",3}
+WorkShop.String[104] = {"Ruud option maximum already reached","Ruud option máximo já atingido","Ruud option máximo ya alcanzado",3}
+WorkShop.String[105] = {"(Ruud Upgrade):","(Ruud Upgrade):","(Ruud actualización):",3}
+WorkShop.String[106] = {" > %d ".._coin.name.WCoinC.." (Rate: %d%%): enter %d for %s"," > %d ".._coin.name.WCoinC.." (Rate: %d%%): insira %d para %s"," > %d ".._coin.name.WCoinC.." (Tasa: %d%%): ingrese %d para %s",1}
+WorkShop.String[107] = {"This item can only be updated when permanent","Esse item só pode ser atualizado quando permanente","Este artículo solo se puede actualizar cuando es permanente",3}
+WorkShop.String[108] = {"You don't have %d ".._coin.name.WCoinC,"Você não tem %d ".._coin.name.WCoinC,"No tienes %d ".._coin.name.WCoinC,3}
+WorkShop.String[109] = {"Item update failed","Falha na atualização do item","Error al actualizar el artículo",3}
+WorkShop.String[110] = {"Ruud option added: %s","Ruud option adicionado: %s","Ruud option agregado: %s",1}
+
+WorkShop.String[111] = {"You cannot update the mastery option of that item","Você não pode atualizar o mastery option deste item","No se puede actualizar el mastery option de ese artículo.",3}
+WorkShop.String[112] = {"Mastery maximum already reached","Mastery máximo já atingido","Mastery máximo ya alcanzado",3}
+WorkShop.String[113] = {"(Mastery Upgrade):","(Mastery Upgrade):","(Mastery actualización):",3}
+WorkShop.String[114] = {" > %d ".._coin.name.WCoinC.." (Rate: %d%%): enter %d for %s"," > %d ".._coin.name.WCoinC.." (Rate: %d%%): insira %d para %s"," > %d ".._coin.name.WCoinC.." (Tasa: %d%%): ingrese %d para %s",1}
+WorkShop.String[115] = {"This item can only be updated when permanent","Esse item só pode ser atualizado quando permanente","Este artículo solo se puede actualizar cuando es permanente",3}
+WorkShop.String[116] = {"You don't have %d ".._coin.name.WCoinC,"Você não tem %d ".._coin.name.WCoinC,"No tienes %d ".._coin.name.WCoinC,3}
+WorkShop.String[117] = {"Item update failed","Falha na atualização do item","Error al actualizar el artículo",3}
+WorkShop.String[118] = {"Matery level upgraded: %s","Matery level atualizado: %s","Matery level actualizado: %s",1}
+
+WorkShop.String[119] = {"The item does not have an life option","O item não possui opção life","El artículo no tiene una life opción.",3}
+WorkShop.String[120] = {"You cannot remove the life from that item","Você não pode remover o life desse item","No puedes quitar lo life de ese artículo.",3}
+WorkShop.String[121] = {"You don't have %d ".._coin.name.WCoinC,"Você não tem %d ".._coin.name.WCoinC,"No tienes %d ".._coin.name.WCoinC,3}
+WorkShop.String[123] = {"Life %s removed","Excllente %s removido","Life %s eliminado",1}
+
+WorkShop.String[124] = {"(Life Remove):","(Life Remove):","(Eliminar Life):",3}
+WorkShop.String[125] = {" > %d ".._coin.name.WCoinC.." - enter %d for +%s"," > %d ".._coin.name.WCoinC.." - insira %d para +%s"," > %d ".._coin.name.WCoinC.." - ingrese %d para +%s",1}
+WorkShop.String[126] = {"This item can only be updated when permanent","Esse item só pode ser atualizado quando permanente","Este artículo solo se puede actualizar cuando es permanente",3}
+
+WorkShop.String[127] = {"You don't have %d ".._coin.name.WCoinC,"Você não tem %d ".._coin.name.WCoinC,"No tienes %d ".._coin.name.WCoinC,3}
+WorkShop.String[128] = {"Life changed to +%s","Life alterado para +%s","Excelente cambiado para +%s",1}
---
+++ WorkShop/Configs_Default/_Config.lua
@@ -0,0 +1,20 @@
+WorkShop.Switch = true -- true > enable | false > disable
+WorkShop.CoinType = 10000 -- Required coin type | 10000 > WCoinC | 10001 > WCoinP | 10002 > GoblinPoint | 10003 > Zen | 10004 > Ruud
+
+--[[ below, command number in CommandManager.txt ]]
+-- Add option command list
+WorkShop.CodeAddLevel = 200
+WorkShop.CodeAddSkill = 201
+WorkShop.CodeAddLuck = 202
+WorkShop.CodeAddLife = 203
+WorkShop.CodeAddExcellent = 204
+WorkShop.CodeAddAncient = 205
+WorkShop.CodeAddHarmony = 206
+WorkShop.CodeAddRefine = 207
+WorkShop.CodeAddSocket = 208
+WorkShop.CodeAddRuud = 209
+WorkShop.CodeAddMastery = 210
+
+-- Remove option command list
+WorkShop.CodeRemoveExcellent = 220
+WorkShop.CodeRemoveLife = 221
