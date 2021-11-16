5d55448b015e9e5026f33a7fb5a284f491f3d45a
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-11-16 18:09:45 -0300

    1_0_0

---
+++ SmTeleport/Configs_Default/Configs.lua
@@ -0,0 +1,8 @@
+SmTeleport.Switch = true;
+SmTeleport.DelayMessage = 1; -- message to user
+SmTeleport.Delay = 1300; -- teleport delay in MS
+SmTeleport.Message = {
+    "[SYSTEM]Teleport Delay: %d", -- eng
+    "[SYSTEM]Teleport Delay: %d", -- por
+    "[SYSTEM]Teleport Delay: %d", -- spn
+}
