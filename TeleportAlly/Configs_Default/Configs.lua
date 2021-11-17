b32bb9d9f3b81b0b7d506249cd08628aeb7cb152
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-11-17 16:28:29 -0300

    1_0_0

---
+++ TeleportAlly/Configs_Default/Configs.lua
@@ -0,0 +1,8 @@
+TeleportAlly.Switch = true;
+TeleportAlly.DelayMessage = 1; -- message to user
+TeleportAlly.Delay = 1300; -- teleport delay in MS
+TeleportAlly.Message = {
+    "[SYSTEM]Teleport Ally Delay: %d", -- eng
+    "[SYSTEM]Teleport Ally Delay: %d", -- por
+    "[SYSTEM]Teleport Ally Delay: %d", -- spn
+}
