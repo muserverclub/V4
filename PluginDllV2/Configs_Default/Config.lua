9c8db3151a9e18bad30841c2f96dfc7b0c436a2f
muserverclub <80699118+muserverclub@users.noreply.github.com>
2022-03-11 20:27:01 -0300

    1_0_0

---
+++ PluginDllV2/Configs_Default/Config.lua
@@ -0,0 +1,160 @@
+PluginDllV2.Switch = true -- true > enabled | false > disabled
+PluginDllV2.AntiInjection = true -- bypass protection
+
+PluginDllV2.IgnoreFlag = {2} -- Flags ignored by account limit | 0 > Online | 1 > OffAttack | 2 > OffStore
+
+PluginDllV2.Limit.Total = -1 -- Total accounts that can be connected per pc
+PluginDllV2.Limit[0] = -1 -- Maximum free accounts per pc
+PluginDllV2.Limit[1] = -1 -- Maximum vip1 accounts per pc
+PluginDllV2.Limit[2] = -1 -- Maximum vip2 accounts per pc
+PluginDllV2.Limit[3] = -1 -- Maximum vip3 accounts per pc
+
+PluginDllV2.Strings.AntiInjection = {
+  "[System] Plugin.dll not found!! Disconecting user...",
+  "[System] Plugin.dll not found!! Disconecting user...",
+  "[System] Plugin.dll not found!! Disconecting user...",
+  1
+}
+
+
+PluginDllV2.Strings.ExceededLimit = {
+  "Limit of connected accounts exceeded",
+  "Limite de contas conectadas excedida",
+  "Se superó el límite de cuentas conectadas",
+  1
+}
+
+PluginDllV2.Strings.ComputerNotAllowed = {
+  "<font size='15' color='#F0FFFF'>Connection from this computer was not allowed.</font>",
+  "<font size='15' color='#F0FFFF'>A conexão a partir deste computador não foi permitida.</font>",
+  "<font size='15' color='#F0FFFF'>No se permitió la conexión desde esta computadora.</font>",
+  0
+}
+PluginDllV2.Strings.NotAllowedFunctions = {
+  "<font size='15' color='#F0FFFF'>Some functions were blocked.</font>",
+  "<font size='15' color='#F0FFFF'>Algumas funções foram bloqueadas.</font>",
+  "<font size='15' color='#F0FFFF'>Algunas funciones fueron bloqueadas.</font>",
+  0
+}
+
+PluginDllV2.Strings.InvalidMachineData = {
+  "Move blocked",
+  "Move bloqueado",
+  "Mover bloqueado",
+  1
+}
+
+PluginDllV2.Strings.Total = {
+  "Contas conectadas: %d / %d",
+  "Contas conectadas: %d / %d",
+  "Cuentas conectadas: %d / %d",
+  1
+}
+PluginDllV2.Strings[0] = {
+  "[Free] connected accounts: %d / %d",
+  "[Free] contas conectadas: %d / %d",
+  "[Free] cuentas conectadas: %d / %d",
+  1
+}
+PluginDllV2.Strings[1] = {
+  "[Vip1] connected accounts: %d / %d",
+  "[Vip1] contas conectadas: %d / %d",
+  "[Vip1] cuentas conectadas: %d / %d",
+  1
+}
+PluginDllV2.Strings[2] = {
+  "[Vip2] connected accounts: %d / %d",
+  "[Vip2] contas conectadas: %d / %d",
+  "[Vip2] cuentas conectadas: %d / %d",
+  1
+}
+PluginDllV2.Strings[3] = {
+  "[Vip3] connected accounts: %d / %d",
+  "[Vip3] contas conectadas: %d / %d",
+  "[Vip3] cuentas conectadas: %d / %d",
+  1
+}
+
+PluginDllV2.Strings[101] = {
+  "Mover item blocked",
+  "Mover item bloqueado",
+  "Mover item bloqueado",
+  1
+}
+
+PluginDllV2.Strings[102] = {
+  "Sold item blocked",
+  "Vender item bloqueado",
+  "Vender item bloqueado",
+  1
+}
+
+PluginDllV2.Strings[103] = {
+  "Buy item blocked",
+  "Comprar item bloqueado",
+  "Comprar item bloqueado",
+  1
+}
+
+PluginDllV2.Strings[104] = {
+  "Use item blocked",
+  "Usar item bloqueado",
+  "Usar item bloqueado",
+  1
+}
+
+PluginDllV2.Strings[105] = {
+  "Drop item blocked",
+  "Dropar item bloqueado",
+  "Dropar item bloqueado",
+  1
+}
+
+PluginDllV2.Strings[106] = {
+  "Pick up item blocked",
+  "Pegar item bloqueado",
+  "Pegar item bloqueado",
+  1
+}
+
+PluginDllV2.Strings[107] = {
+  "Trade blocked",
+  "Troca bloqueado",
+  "Troca bloqueado",
+  1
+}
+
+PluginDllV2.Strings[108] = {
+  "Open shop blocked",
+  "Abrir loja bloqueado",
+  "Abrir loja bloqueado",
+  1
+}
+
+PluginDllV2.Strings[109] = {
+  "Chaos Machine blocked",
+  "Chaos Machine bloqueado",
+  "Chaos Machine bloqueado",
+  1
+}
+
+PluginDllV2.Strings[110] = {
+  "Cashshop blocked",
+  "Cashshop bloqueado",
+  "Cashshop bloqueado",
+  1
+}
+
+PluginDllV2.Strings[111] = {
+  "Chat blocked",
+  "Chat bloqueado",
+  "Chat bloqueado",
+  1
+}
+
+PluginDllV2.Strings[112] = {
+  "Move blocked",
+  "Move bloqueado",
+  "Move bloqueado",
+  1
+}
