faa240f338fac5ca18d1f730f5b0c78f6067930e
muserverclub <80699118+muserverclub@users.noreply.github.com>
2022-03-14 19:27:53 -0300

    1_0_1

--- InfinityArrows/Configs_Default/Configs.lua
+++ InfinityArrows/Configs_Default/Configs.lua
@@ -1,6 +1,9 @@
 InfinityArrows.Switch = true;
 InfinityArrows.CheckTime = 5;
-InfinityArrows.MinArrow = 5;
+InfinityArrows.MinArrow = 255;
+InfinityArrows.MinBolt = 255;
+InfinityArrows.Arrow = true;
+InfinityArrows.Bolt = true;
 InfinityArrows.GiveArrows = {
     ItemGive = {
         { -- inicio
@@ -20,6 +23,33 @@ InfinityArrows.GiveArrows = {
         { -- inicio
             Section = 4,	-- Categoria no item.txt
             ID = 15,			-- ID da Categoria no item.txt
+            Level = 0,		-- Nível ( 0 a 15 )
+            Durability = 255,	-- Durabilidade ( deixe 0 para usar o padrão do item.txt )            
+        }, --duplicar até aqui para adicionar mais
+
+        
+    }
+}
+
+InfinityArrows.GiveBolts = {
+    ItemGive = {
+        { -- inicio
+            Section = 4,	-- Categoria no item.txt
+            ID = 7,			-- ID da Categoria no item.txt
+            Level = 0,		-- Nível ( 0 a 15 )
+            Durability = 255,	-- Durabilidade ( deixe 0 para usar o padrão do item.txt )            
+        }, --duplicar até aqui para adicionar mais
+
+        { -- inicio
+            Section = 4,	-- Categoria no item.txt
+            ID = 7,			-- ID da Categoria no item.txt
+            Level = 0,		-- Nível ( 0 a 15 )
+            Durability = 255,	-- Durabilidade ( deixe 0 para usar o padrão do item.txt )            
+        }, --duplicar até aqui para adicionar mais
+
+        { -- inicio
+            Section = 4,	-- Categoria no item.txt
+            ID = 7,			-- ID da Categoria no item.txt
             Level = 0,		-- Nível ( 0 a 15 )
             Durability = 255,	-- Durabilidade ( deixe 0 para usar o padrão do item.txt )            
         }, --duplicar até aqui para adicionar mais
