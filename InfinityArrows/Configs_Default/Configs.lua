d0d03f6b9fb8ef27d34b9efbfe664fa21e02dc61
muserverclub <80699118+muserverclub@users.noreply.github.com>
2022-03-14 12:27:29 -0300

    1_0_0

---
+++ InfinityArrows/Configs_Default/Configs.lua
@@ -0,0 +1,29 @@
+InfinityArrows.Switch = true;
+InfinityArrows.CheckTime = 5;
+InfinityArrows.MinArrow = 5;
+InfinityArrows.GiveArrows = {
+    ItemGive = {
+        { -- inicio
+            Section = 4,	-- Categoria no item.txt
+            ID = 15,			-- ID da Categoria no item.txt
+            Level = 0,		-- Nível ( 0 a 15 )
+            Durability = 255,	-- Durabilidade ( deixe 0 para usar o padrão do item.txt )            
+        }, --duplicar até aqui para adicionar mais
+
+        { -- inicio
+            Section = 4,	-- Categoria no item.txt
+            ID = 15,			-- ID da Categoria no item.txt
+            Level = 0,		-- Nível ( 0 a 15 )
+            Durability = 255,	-- Durabilidade ( deixe 0 para usar o padrão do item.txt )            
+        }, --duplicar até aqui para adicionar mais
+
+        { -- inicio
+            Section = 4,	-- Categoria no item.txt
+            ID = 15,			-- ID da Categoria no item.txt
+            Level = 0,		-- Nível ( 0 a 15 )
+            Durability = 255,	-- Durabilidade ( deixe 0 para usar o padrão do item.txt )            
+        }, --duplicar até aqui para adicionar mais
+
+        
+    }
+}
