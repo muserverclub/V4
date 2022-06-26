d80ca5b52da168de996a1d3ade8eea61b3c0a3fa
muserverclub <80699118+muserverclub@users.noreply.github.com>
2022-06-26 19:08:37 -0300

    1_0_0

---
+++ MasterResetBuff/Configs/Config.lua
@@ -0,0 +1,93 @@
+MasterResetBuff.Switch = true -- true > enable | false > disable
+
+MasterResetBuff.CommandCode = 1100
+
+MasterResetBuff.Strings.CommandTitle = {"Master Resets: %s","Master Resets: %s","Master Resets: %s",1}
+
+MasterResetBuff.MasterReset[0] = {
+    { ID = 45, Value = 0, Title = {"Aumento de Defesa: +%s","Aumento de Defesa: +%s","Aumento de Defesa: +%s",1} },
+    { ID = 46, Value = 0, Title = {"Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s",1} },
+    { ID = 47, Value = 0, Title = {"Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s",1} },
+    { ID = 48, Value = 0, Title = {"Aumento de Vida: +%s","Aumento de Vida: +%s","Aumento de Vida: +%s",1} },
+    { ID = 49, Value = 0, Title = {"Aumento de Mana: +%s","Aumento de Mana: +%s","Aumento de Mana: +%s",1} },
+}
+
+MasterResetBuff.MasterReset[1] = {
+    { ID = 45, Value = 440, Title = {"Aumento de Defesa: +%s","Aumento de Defesa: +%s","Aumento de Defesa: +%s",1} },
+    { ID = 46, Value = 440, Title = {"Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s",1} },
+    { ID = 47, Value = 440, Title = {"Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s",1} },
+    { ID = 48, Value = 1050, Title = {"Aumento de Vida: +%s","Aumento de Vida: +%s","Aumento de Vida: +%s",1} },
+    { ID = 49, Value = 350, Title = {"Aumento de Mana: +%s","Aumento de Mana: +%s","Aumento de Mana: +%s",1} },
+}
+
+MasterResetBuff.MasterReset[2] = {
+    { ID = 45, Value = 880, Title = {"Aumento de Defesa: +%s","Aumento de Defesa: +%s","Aumento de Defesa: +%s",1} },
+    { ID = 46, Value = 880, Title = {"Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s",1} },
+    { ID = 47, Value = 880, Title = {"Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s",1} },
+    { ID = 48, Value = 1550, Title = {"Aumento de Vida: +%s","Aumento de Vida: +%s","Aumento de Vida: +%s",1} },
+    { ID = 49, Value = 650, Title = {"Aumento de Mana: +%s","Aumento de Mana: +%s","Aumento de Mana: +%s",1} },
+}
+
+MasterResetBuff.MasterReset[3] = {
+    { ID = 45, Value = 1320, Title = {"Aumento de Defesa: +%s","Aumento de Defesa: +%s","Aumento de Defesa: +%s",1} },
+    { ID = 46, Value = 1320, Title = {"Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s",1} },
+    { ID = 47, Value = 1320, Title = {"Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s",1} },
+    { ID = 48, Value = 2050, Title = {"Aumento de Vida: +%s","Aumento de Vida: +%s","Aumento de Vida: +%s",1} },
+    { ID = 49, Value = 950, Title = {"Aumento de Mana: +%s","Aumento de Mana: +%s","Aumento de Mana: +%s",1} },
+}
+
+MasterResetBuff.MasterReset[4] = {
+    { ID = 45, Value = 1740, Title = {"Aumento de Defesa: +%s","Aumento de Defesa: +%s","Aumento de Defesa: +%s",1} },
+    { ID = 46, Value = 1740, Title = {"Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s",1} },
+    { ID = 47, Value = 1740, Title = {"Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s",1} },
+    { ID = 48, Value = 2550, Title = {"Aumento de Vida: +%s","Aumento de Vida: +%s","Aumento de Vida: +%s",1} },
+    { ID = 49, Value = 1250, Title = {"Aumento de Mana: +%s","Aumento de Mana: +%s","Aumento de Mana: +%s",1} },
+}
+
+MasterResetBuff.MasterReset[5] = {
+    { ID = 45, Value = 2260, Title = {"Aumento de Defesa: +%s","Aumento de Defesa: +%s","Aumento de Defesa: +%s",1} },
+    { ID = 46, Value = 2260, Title = {"Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s",1} },
+    { ID = 47, Value = 2260, Title = {"Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s",1} },
+    { ID = 48, Value = 3550, Title = {"Aumento de Vida: +%s","Aumento de Vida: +%s","Aumento de Vida: +%s",1} },
+    { ID = 49, Value = 1550, Title = {"Aumento de Mana: +%s","Aumento de Mana: +%s","Aumento de Mana: +%s",1} },
+}
+
+MasterResetBuff.MasterReset[6] = {
+    { ID = 45, Value = 2680, Title = {"Aumento de Defesa: +%s","Aumento de Defesa: +%s","Aumento de Defesa: +%s",1} },
+    { ID = 46, Value = 2680, Title = {"Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s",1} },
+    { ID = 47, Value = 2680, Title = {"Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s",1} },
+    { ID = 48, Value = 4050, Title = {"Aumento de Vida: +%s","Aumento de Vida: +%s","Aumento de Vida: +%s",1} },
+    { ID = 49, Value = 1850, Title = {"Aumento de Mana: +%s","Aumento de Mana: +%s","Aumento de Mana: +%s",1} },
+}
+
+MasterResetBuff.MasterReset[7] = {
+    { ID = 45, Value = 3120, Title = {"Aumento de Defesa: +%s","Aumento de Defesa: +%s","Aumento de Defesa: +%s",1} },
+    { ID = 46, Value = 3120, Title = {"Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s",1} },
+    { ID = 47, Value = 3120, Title = {"Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s",1} },
+    { ID = 48, Value = 5050, Title = {"Aumento de Vida: +%s","Aumento de Vida: +%s","Aumento de Vida: +%s",1} },
+    { ID = 49, Value = 2150, Title = {"Aumento de Mana: +%s","Aumento de Mana: +%s","Aumento de Mana: +%s",1} },
+}
+
+MasterResetBuff.MasterReset[8] = {
+    { ID = 45, Value = 3540, Title = {"Aumento de Defesa: +%s","Aumento de Defesa: +%s","Aumento de Defesa: +%s",1} },
+    { ID = 46, Value = 3540, Title = {"Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s",1} },
+    { ID = 47, Value = 3540, Title = {"Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s",1} },
+    { ID = 48, Value = 6550, Title = {"Aumento de Vida: +%s","Aumento de Vida: +%s","Aumento de Vida: +%s",1} },
+    { ID = 49, Value = 2450, Title = {"Aumento de Mana: +%s","Aumento de Mana: +%s","Aumento de Mana: +%s",1} },
+}
+
+MasterResetBuff.MasterReset[9] = {
+    { ID = 45, Value = 4160, Title = {"Aumento de Defesa: +%s","Aumento de Defesa: +%s","Aumento de Defesa: +%s",1} },
+    { ID = 46, Value = 4160, Title = {"Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s",1} },
+    { ID = 47, Value = 4160, Title = {"Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s",1} },
+    { ID = 48, Value = 8550, Title = {"Aumento de Vida: +%s","Aumento de Vida: +%s","Aumento de Vida: +%s",1} },
+    { ID = 49, Value = 2750, Title = {"Aumento de Mana: +%s","Aumento de Mana: +%s","Aumento de Mana: +%s",1} },
+}
+
+MasterResetBuff.MasterReset[10] = {
+    { ID = 45, Value = 4750, Title = {"Aumento de Defesa: +%s","Aumento de Defesa: +%s","Aumento de Defesa: +%s",1} },
+    { ID = 46, Value = 4750, Title = {"Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s","Aumento de Dano Físico: +%s",1} },
+    { ID = 47, Value = 4750, Title = {"Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s","Aumento de Dano Mágico: +%s",1} },
+    { ID = 48, Value = 10500, Title = {"Aumento de Vida: +%s","Aumento de Vida: +%s","Aumento de Vida: +%s",1} },
+    { ID = 49, Value = 3050, Title = {"Aumento de Mana: +%s","Aumento de Mana: +%s","Aumento de Mana: +%s",1} },
+}
