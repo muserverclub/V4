ce0b857de80df76042d734988514c5e126fa0dd8
muserverclub <80699118+muserverclub@users.noreply.github.com>
2022-07-20 19:03:22 -0300

    1_0_0

---
+++ ResetSkill/Configs_Default/Config.lua
@@ -0,0 +1,85 @@
+ResetSkill.Switch = true -- true > enable | false > disable
+
+ResetSkill.CommandMS.Code = 125 -- Comando reset master skill
+
+ResetSkill.CommandExt.Code = 126 -- Comando reset master skill ext
+
+ResetSkill.RemoveBuffList = {
+    1,
+    2,
+    4,
+    5,
+    6,
+    8,
+    9,
+    50,
+    51,
+    52,
+    53,
+    54,
+    71,
+    81,
+    82,
+    129,
+    130,
+    131,
+    134,
+    135,
+    136,
+    138,
+    139,
+    140,
+    143,
+    148,
+    149,
+    150,
+    151,
+    152,
+    153,
+    154,
+    155,
+    165,
+    216,
+    217,
+    218,
+    219,
+    220,
+    221,
+    222,
+    223,
+    224,
+    225,
+    226,
+    293,
+    294,
+    295,
+    296,
+    298,
+    299
+}
+-- List of effects that are removed when the user uses one of the commands
+
+ResetSkill.Strings.MasterSkillReseted = {
+    "[Reset MS] Your Master Skill points have been reset.",
+    "[Reset MS] Seus pontos de master skill foram redefinidos.",
+    "[Reset MS] Tus puntos de habilidad maestra se han restablecido.",
+    1
+}
+ResetSkill.Strings.MasterSkillInvalid = {
+    "[Reset MS] You cannot use this command.",
+    "[Reset MS] Você não pode usar este comando.",
+    "[Reset MS] No puedes usar este comando.",
+    3
+}
+ResetSkill.Strings.MasterSkillExtReseted = {
+    "[Reset Ext] Your Master Skill Ext points have been reset.",
+    "[Reset Ext] Seus pontos de master skill ext foram redefinidos.",
+    "[Reset Ext] Tus puntos de Master Skill Ext se han reiniciado.",
+    1
+}
+ResetSkill.Strings.MasterSkillInvalid = {
+    "[Reset Ext] You cannot use this command.",
+    "[Reset Ext] Você não pode usar este comando.",
+    "[Reset Ext] No puedes usar este comando.",
+    3
+}
